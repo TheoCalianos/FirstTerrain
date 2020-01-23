2f2ee0a9189c2a36ac5ad25501e7a0a9ff0e9812
Theo Calianos <43078567+TheoCalianos@users.noreply.github.com>
2020-01-22 20:14:58 -0500

    made custom tags for editor

---
+++ Assets/Scripts/CustomTerrain.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CustomTerrain : MonoBehaviour
+{
+
+    void Awake()
+    {
+      SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+      SerializedProperty tagsProp = tagManager.FindProperty("tags");
+
+      AddTag(tagsProp, "Terrain");
+      AddTag(tagsProp, "Cloud");
+      AddTag(tagsProp, "Shore");
+
+      tagManager.ApplyModifiedProperties();
+
+      this.gameObject.tag = "Terrain";
+    }
+
+    void AddTag(SerializedProperty tagsProp,string newTag)
+    {
+      bool found = false;
+
+      for (int i = 0; i < tagsProp.arraySize; i++)
+      {
+        SerializedProperty t = tagsProp.GetArrayElementAtIndex(i);
+        if(t.stringValue.Equals(newTag))
+        {
+            found = true;
+            break;
+        }
+      }
+      if(!found)
+      {
+        tagsProp.InsertArrayElementAtIndex(0);
+        SerializedProperty newTagProp = tagsProp.GetArrayElementAtIndex(0);
+        newTagProp.stringValue = newTag;
+      }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
